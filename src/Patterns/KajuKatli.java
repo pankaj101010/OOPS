84b3d891adb997e1662aa550c8c31641ca374dd8
bharadiya <shashank.bharadiya@gmail.com>
2019-04-02 21:07:23 +0530

    Added new Java Program of pattern making according to user input named file as Kajukatli.java

---
+++ src/Patterns/KajuKatli.java
@@ -0,0 +1,39 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class KajuKatli {
+	private static Scanner inp;
+
+	public static void main(String[] args) {
+		inp = new Scanner(System.in);
+		System.out.println("Please enter a odd number of your choice to design kaju Katli");
+		int n = inp.nextInt();
+		while (true) {
+			if (n % 2 == 0) {
+				System.err.println("Please enter an odd number only");
+				n = inp.nextInt();
+			} else {
+				break;
+			}
+		}
+		for (int i = 1; i <= n; i++) {
+			if (i <= (n / 2 + 1)) {
+				for (int s = 1; s <= (n - i); s++) {
+					System.out.print(" ");
+				}
+				for (int j = 1; j <= i; j++) {
+					System.out.print("$ ");
+				}
+			} else {
+				for (int s = 1; s <= i - 1; s++) {
+					System.out.print(" ");
+				}
+				for (int j = 1; j <= n - i + 1; j++) {
+					System.out.print("$ ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
--- src/Patterns/Pattern1.java
+++ src/Patterns/Pattern1.java
@@ -3,21 +3,16 @@ package Patterns;
 public class Pattern1 {
 	public static void main(String[] args) {
 		int n = 6;
-		char z = 96;
-		for (int i = 1; i <= n; i++) { // For printing n lines
-			for (int space = 1; space <= n - i; space++) {
+		int k=1;
+		for (int i = 1; i <= n; i++) {
+			for (int space = 1; space <= n-i; space++) {
 				System.out.print(" ");
 			}
-			if (i % 2 != 0)
-				z++;
 			for (int j = 1; j <= i; j++) {
-				if (i % 2 == 0) {
-					System.out.print(i / 2 + " ");
-				} else {
-					System.out.print(z + " ");
-				}
+				System.out.print(k+" ");
+				k++;
 			}
-			System.out.println();
+			System.out.println();  
 		}
 	}
 }
